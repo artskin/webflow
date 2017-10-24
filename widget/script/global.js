3f7eec96b9cf8982e5bf4668d8815e5a3753ee66
artskin <artskin@163.com>
2017-10-24 19:26:01 +0800

    ios push

--- widget/script/global.js
+++ widget/script/global.js
@@ -3,21 +3,30 @@
  * Copyright (c) 2017 artskin
  */
 (function(window){
-  apiready = function(){
-    api.setRefreshHeaderInfo({
-      visible: true,
-      loadingImg: 'widget://image/refresh.png',
-      bgColor: 'rgba(0,0,0,0)',
-      textColor: 'rgba(255,255,255,0.5)',
-      textDown: '下拉刷新',
-      textUp: '释放刷新'
-    }, function(ret, err){
-          loadData();
-            app();
-    });
-    api.addEventListener({name: 'scrolltobottom'}, function(ret, err){
-        loadData();
-    });
-  };
+
 
 })(window);
+
+apiready = function(){
+  
+};
+
+function toWeb(obj){
+    var url = $api.attr(obj,'data-url') ||'';
+    var title = $api.attr(obj,'data-title');
+    var frameName = $api.attr(obj,'data-frameName');
+    console.log(title,url)
+    var options = {
+            title: title,
+            url: url,
+            frameName: frameName
+        };
+    options = JSON.stringify(options);
+
+    // api.alert({msg:options});
+    var str = 'indexToWeb('+options+')';
+    api.execScript({
+        name: 'root',
+        script: str
+    });
+};
